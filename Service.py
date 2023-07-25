63ec0bb961768544533f2cb007b724c67fabdb61
Viren <virenkadam2002@gmail.com>
2023-07-25 13:06:56 +0530

    requirements file updates 2

--- Service.py
+++ Service.py
@@ -10,7 +10,7 @@
 import json
 import openai
 
-API_KEY = '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'
+API_KEY = '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'
 openai.api_key = API_KEY
 
 model_id = 'gpt-3.5-turbo'
@@ -41,59 +41,4 @@ def generate_travel_plan(user_input):
 
     print(formatted_content)
     return formatted_content
-# def generate_travel_plan(user_input):
-#     json_input = json.loads(user_input)
-#
-#     city = json_input["city"]
-#     duration = json_input["duration"]
-#
-#     preferences = json_input["preferences"]
-#
-#     plan = f"I'm in {city} for {duration}\t"
-#
-#     for preference in preferences:
-#         plan += f"I like {preference}, "
-#
-#     conversation = [
-#         {"role": "system", "content": plan + "Generate a day wise travel plan in short."}
-#     ]
-#
-#     response = openai.ChatCompletion.create(model=model_id, messages=conversation) # sending conversation list to OpenAI's chat API
-#
-#     content = response['choices'][0]['message']['content']
-#     formatted_content = content.replace("\n", " ")
-#
-#     # Split the content into individual days based on the "Day" pattern
-#     days = formatted_content.split("Day ")
-#
-#     # Remove empty strings from the list
-#     days = [day.strip() for day in days if day.strip()]
-#
-#     return days
-
-# def generate_travel_plan(user_input):
-#     json_input = json.loads(user_input)
-#
-#     city = json_input["city"]
-#     duration = json_input["duration"]
-#
-#     preferences = json_input["preferences"]
-#     # avoidances = json_input["avoidances"]
-#
-#     plan = f"I'm in {city} for {duration}\t"
-#
-#     for preference in preferences:
-#         plan += f"I like {preference}, "
-#
-#     # for avoidance in avoidances:
-#     #     plan += f"I wish to avoid {avoidance}, "
-#
-#     conversation = [
-#         {"role": "system", "content": plan + "Generate a travel plan."}
-#     ]
-#
-#     response = openai.ChatCompletion.create(model=model_id, messages=conversation)
-#
-#     generated_response = response['choices'][0]['message']['content']
-#     return generated_response
 
